5ce51438743f0845fe40ce8440bbeaf521e1b659
Venkata Sabbella <vsabbella@live.com>
2020-03-29 10:00:23 -0700

    db url

--- src/main/java/com/myspringboot/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/myspringboot/conferencedemo/config/PersistenceConfiguration.java
@@ -9,13 +9,13 @@ import javax.sql.DataSource;
 @Configuration
 public class PersistenceConfiguration {
 
-    @Bean
-    public DataSource dataSource(){
-
-        DataSourceBuilder builder =  DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/conference_app");
-        System.out.println("My custom datasource has been initialized and  and set");
-        return builder.build();
-    }
+//    @Bean
+//    public DataSource dataSource(){
+//
+//        DataSourceBuilder builder =  DataSourceBuilder.create();
+//        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+//        System.out.println("My custom datasource has been initialized and  and set");
+//        return builder.build();
+//    }
 
 }
